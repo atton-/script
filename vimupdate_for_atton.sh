9dd81772196ce2e9f9c3fbdfb1224f059e71c6f2
e115763 <e115763@gmail.com>
2012-01-28 09:39:14 +0900

    add vimupdate_for_atton.sh : enable pythoninterp

---
+++ vimupdate_for_atton.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# vim install by mercurial and customized for atton
+mkdir work
+cd work
+hg clone https://vim.googlecode.com/hg/ vim
+cd vim
+./configure --enable-multibyte --enable-pythoninterp=yes
+make
+make install
+cd ../../
+rm -rf work
